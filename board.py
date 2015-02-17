ab14dae22036856f14a4474124c8516d28d84633
Austin Heiman <atheimanksu@gmail.com>
2015-02-17 08:56:05 -0600

    finished product

--- board.py
+++ board.py
@@ -39,17 +39,46 @@ class Board(object):
     def get_input(self):
         """Return a tuple of (row, col)
         """
-        raw = raw_input("Which square do you choose? (row,col) (Ex: \"1, 2\") ").strip()
-        row = int(raw.split(',')[0]) - 1
-        col = int(raw.split(',')[1]) - 1
-        if not self.rows[row][col].strip():
-            return (row, col)
-        else:
-            raise Exception('{},{} is already occupied'.format(row, col))
+        raw = raw_input(
+            "Which square do you choose? (row,col) (Ex: \"1,2\") "
+        ).strip()
+        try:
+            row = int(raw.split(',')[0]) - 1
+            col = int(raw.split(',')[1]) - 1
+        except ValueError as e:
+            raise SystemExit(
+                "row and col should be integers.\n"
+                "Way to go {t}'s, you ruin all the fun.".format(t=self.turn,
+                                                                e=e,)
+            )
+        except IndexError as e:
+            raise SystemExit(
+                "Need both a row and a column.\n"
+                "Way to go {t}'s, you ruin all the fun.".format(t=self.turn,
+                                                                e=e,)
+            )
+        try:
+            if not self.rows[row][col].strip():
+                return (row, col)
+            else:
+                raise SystemExit(
+                    "row {r}, col {c} is already occupied.\n"
+                    "Way to go {t}'s, you ruin all the fun.".format(
+                        r=row,
+                        c=col,
+                        t=self.turn,
+                    )
+                )
+        except IndexError as e:
+            raise SystemExit(
+                "maximum row and col value is 3.\n"
+                "Way to go {t}'s, you ruin all the fun.".format(t=self.turn,
+                                                                e=e)
+            )
 
 
     def turn_cycle(self):
-        print "\nYou're up, {turn}'s".format(turn=self.turn)
+        print "You're up, {turn}'s".format(turn=self.turn)
         row, col = self.get_input()
         self.rows[row][col] = self.turn
         self.draw()
@@ -61,10 +90,29 @@ class Board(object):
     def check_winner(self):
         """Set self.winner to True when someone has won the game.
         """
-        pass
+        winning_sets = [
+            ((0,0), (0,1), (0,2)),
+            ((1,0), (1,1), (1,2)),
+            ((2,0), (2,1), (2,2)),
+            ((0,0), (1,0), (2,0)),
+            ((0,1), (1,1), (2,1)),
+            ((0,2), (1,2), (2,2)),
+            ((0,0), (1,1), (2,2)),
+            ((0,2), (1,1), (2,0)),
+        ]
+        for s in winning_sets:
+            spaces = []
+            for space in s:
+                row = space[0]
+                col = space[1]
+                spaces.append(self.rows[row][col])
+            if spaces[0] == spaces[1] == spaces[2] and spaces[0] != ' ':
+                self.winner = spaces[0]
 
 
 if __name__ == "__main__":
     board = Board()
+    board.draw()
     while not board.winner:
         board.turn_cycle()
+    print '\n***\nWe have a winner! {w}\n***'.format(w=board.winner)
